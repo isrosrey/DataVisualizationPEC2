de00c8b427143793a77b4bce058d0308d7c21624
isrosrey <isrosrey@uoc.edu>
2022-04-15 14:14:56 +0200

    Añadir código de radarChart

--- RadarChart.py
+++ RadarChart.py
@@ -1,50 +1,56 @@
-# https://python-graph-gallery.com/390-basic-radar-chart
+# Ejemplo de código original obtenido en: https://python-graph-gallery.com/390-basic-radar-chart
 
-# Libraries
+# Librerías necesarias
 import matplotlib.pyplot as plt
 import pandas as pd
 from math import pi
- 
-# Set data
-df = pd.DataFrame({
-'group': ['A','B','C','D'],
-'var1': [38, 1.5, 30, 4],
-'var2': [29, 10, 9, 34],
-'var3': [8, 39, 23, 24],
-'var4': [7, 31, 33, 14],
-'var5': [28, 15, 32, 14]
-})
- 
-# number of variable
-categories=list(df)[1:]
+
+# Se leen los datos del CSV obtenido del INE: https://www.ine.es/up/kBpWj4DtiD
+temp = pd.read_csv('https://raw.githubusercontent.com/isrosrey/DataVisualizationPEC2/main/data/Poblacion6Municipios.csv',
+                    sep=';', encoding='ansi', thousands='.')
+
+# Se elimina el código postal
+temp["Municipios"] = temp["Municipios"].str.extract('\s(.*)')
+
+# Se eliminan datos de Sexo y Periodo que no se usarán
+temp = temp.drop(['Sexo', 'Periodo'], axis=1)
+# Se transponen los datos para tener una columna con cada municipio
+temp = temp.T
+# Se asignan los municipios como cabecera de la tabla
+temp.columns = temp.iloc[0]
+# Se elimina la fila con los nombres de los municipios que sería redundante
+temp = temp.drop(temp.index[0])
+
+# cantidad de variables
+categories=list(temp)[0:]
 N = len(categories)
- 
-# We are going to plot the first line of the data frame.
-# But we need to repeat the first value to close the circular graph:
-values=df.loc[0].drop('group').values.flatten().tolist()
-values += values[:1]
+
+values = temp.iloc[0].to_list()
+values.append(values[0])
 values
  
-# What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+# Se calcula el ángulo de cada eje en el gráfico (Se divide el gráfico entre el número de variables)
 angles = [n / float(N) * 2 * pi for n in range(N)]
 angles += angles[:1]
  
-# Initialise the spider plot
+# Se inicializa el gráfico de radar
 ax = plt.subplot(111, polar=True)
  
-# Draw one axe per variable + add labels
-plt.xticks(angles[:-1], categories, color='grey', size=8)
+# Dibuja un eje por cada variable y se añaden las etiquetas
+plt.xticks(angles[:-1], categories, color='blue', size=10)
  
-# Draw ylabels
+# Se añaden las etiquetas del eje Y
 ax.set_rlabel_position(0)
-plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
-plt.ylim(0,40)
+plt.yticks([5000,15000,25000],
+           ["5000","15000","25000"],
+           color="grey", size=7)
+plt.ylim(0,35000)
  
-# Plot data
+# Se pintan los datos
 ax.plot(angles, values, linewidth=1, linestyle='solid')
  
-# Fill area
+# Se rellena el área
 ax.fill(angles, values, 'b', alpha=0.1)
 
-# Show the graph
+# Se muestra el gráfico
 plt.show()
